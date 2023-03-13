71e9706c0359e3e4918876732509ea31b49fcb96
mark mwendwa <mwendwamark310@gmail.com>
2023-03-13 16:39:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+
+const companyName="Scuber"
+
+
+
+let mostProfitableNeighborhood="Chelsea"
+mostProfitableNeighborhood="Chelsea"
+let companyCeo="Susan Smith"
+companyCeo="Susan Smith"
